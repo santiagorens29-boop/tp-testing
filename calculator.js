aff003e488cacfba68f6582ca6d0de26a88e78b8
Santiago Rens <santiagorens29@gmail.com>
2026-04-22 19:39:24 -0300

    Alfa: Archivos de calculadora y test iniciales

---
+++ calculator.js
@@ -0,0 +1,7 @@
+export function add(a, b) {
+  return a + b; // Implementación correcta de Alfa [cite: 67]
+}
+
+export function subtract(a, b) {
+  // Este espacio lo dejamos para que Beta lo complete en la Fase 2 [cite: 43]
+}
---
+++ calculator.test.js
@@ -0,0 +1,5 @@
+import { add } from './calculator'; [cite: 80]
+
+test('Alfa: add(1, 2) should return 3', () => {
+  expect(add(1, 2)).toBe(3); [cite: 81, 83]
+});
